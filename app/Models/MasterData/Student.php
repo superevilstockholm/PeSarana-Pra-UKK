5f807cdb06ab643c1f4baa19a21794aa0e082459
superevilstockholm <balilombok11@gmail.com>
2026-02-02 21:27:40 +0700

    Created Students Table (migration and model)

---
+++ app/Models/MasterData/Student.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\MasterData;
+
+use Illuminate\Database\Eloquent\Model;
+
+// Models
+use App\Models\User;
+use App\Models\MasterData\Classroom;
+
+class Student extends Model
+{
+    protected $table = 'students';
+
+    protected $fillable = [
+        'nisn',
+        'name',
+        'dob',
+        'classroom_id',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'dob' => 'date'
+    ];
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
