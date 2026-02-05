7665a9f3e83dae938e3c778592f881efe6552280
superevilstockholm <balilombok11@gmail.com>
2026-02-05 22:44:42 +0700

    Created delete cover image path field from aspiration table migration and updated model

--- app/Models/MasterData/Aspiration.php
+++ app/Models/MasterData/Aspiration.php
@@ -11,14 +11,12 @@ use App\Models\MasterData\AspirationImage;
 
 // Enums
 use App\Enums\AspirationStatusEnum;
-use Illuminate\Support\Facades\Storage;
 
 class Aspiration extends Model
 {
     protected $table = 'aspirations';
 
     protected $fillable = [
-        'cover_image_path',
         'title',
         'content',
         'location',
@@ -30,17 +28,6 @@ class Aspiration extends Model
     protected $casts = [
         'status' => AspirationStatusEnum::class,
     ];
-
-    protected $appends = [
-        'cover_image_path_url'
-    ];
-
-    public function getCoverImagePathUrlAttribute(): string
-    {
-        return $this->cover_image_path
-            ? Storage::url($this->cover_image_path)
-            : asset('static/img/no-image-placeholder.svg');
-    }
 
     public function student()
     {
