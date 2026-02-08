be60a8dcb53ea4f6007718c9cb6642957f88ab1d
superevilstockholm <balilombok11@gmail.com>
2026-02-08 19:50:19 +0700

    Fix bug sidebar helper

--- app/Helpers/SidebarHelper.php
+++ app/Helpers/SidebarHelper.php
@@ -8,8 +8,8 @@ if (!function_exists('adminSidebarItems')) {
                 ['label' => 'dashboard', 'icon' => 'ti ti-dashboard', 'route' => 'dashboard.admin.index', 'active_pattern' => 'dashboard.admin.index'],
             ],
             'master data' => [
-                ['label' => 'categories', 'icon' => 'ti ti-tag', 'route' => 'dashboard.admin.master-data.categories.index', 'active_pattern' => 'dashboard.admin.master-data.categories.*   '],
-                ['label' => 'aspirations', 'icon' => 'ti ti-flag', 'route' => 'dashboard.admin.master-data.aspirations.index', 'active_pattern' => 'dashboard.admin.master-data.aspirations.*   '],
+                ['label' => 'categories', 'icon' => 'ti ti-tag', 'route' => 'dashboard.admin.master-data.categories.index', 'active_pattern' => 'dashboard.admin.master-data.categories.*'],
+                ['label' => 'aspirations', 'icon' => 'ti ti-flag', 'route' => 'dashboard.admin.master-data.aspirations.index', 'active_pattern' => 'dashboard.admin.master-data.aspirations.*'],
             ],
         ];
         return $items;
